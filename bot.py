7f31b1780e950130eb797e4fc29c91d29fdb90c3
DesCyberBas <33094373+Basza112@users.noreply.github.com>
2019-05-09 22:30:02 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
-Import os, sys, time
+import os, sys, time
 
 def restart_program():
         python = sys.executable
         os.execl(python, python, * sys.argv)
         curdir = os.getcwd()
-bot = raw_input ("You : )
+
+bot = raw_input ("You : ")
 
 if bot == 3:
         os.system ("cat support.txt")
