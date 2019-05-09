a92cb78653b9abe87845ae9472800ccc015523d7
DesCyberBas <33094373+Basza112@users.noreply.github.com>
2019-05-09 22:22:05 +0700

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,13 @@
+Import os, sys, time
+
+def restart_program():
+        python = sys.executable
+        os.execl(python, python, * sys.argv)
+        curdir = os.getcwd()
+bot = raw_input ("You : )
+
+if bot == 3:
+        os.system ("cat support.txt")
+else:
+        os.system ("cat support.txt")
+        restart_program()
